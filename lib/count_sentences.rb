91bcff834828a53d98c5d0c357c968347dea1e54
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-30 20:49:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "I love coding.".end_with?(".")
   end
 
   def question?
-
+    "What are you doing?".end_with?("?")
   end
 
   def exclamation?
-
+  "Hello Abby!".end_with?("!")
   end
 
   def count_sentences
-
+  "I love to code. I recently started. I am not doing so bad at it".count_sentences
   end
 end
